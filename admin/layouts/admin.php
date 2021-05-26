28ca8f7a181b45f1f2f6ff34ffb067d1abbad433
Neudo <bassalair.quentin@gmail.com>
2021-05-26 13:51:21 +0200

    Ajout WYSIWYG + css allPosts

--- admin/layouts/admin.php
+++ admin/layouts/admin.php
@@ -3,10 +3,12 @@
     <head>
         <meta charset="utf-8">
         <link href="./assets/sass/admin.css" rel="stylesheet">
+
     </head>
     <body>
         <?php require_once ('partials/header.php'); ?>
         <?php require_once ('partials/menu.php'); ?>
+    
 
 
         <?php if(isset($_SESSION['message'])):?> 
@@ -21,4 +23,6 @@
             <?php require_once($view); ?>
         </main>
     </body>
+    <script src="https://cdn.tiny.cloud/1/70bt1iuu0fg16xu3wshagcsi6ip9vks1yxlomgq2ukxyemy9/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+    <script>tinymce.init({selector: '.tinymce', skin: 'oxide-dark',});</script>
 </html>
